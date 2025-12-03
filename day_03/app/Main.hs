a5f32dd10eff9a43a86cab5a27cdf5be4e9a17ff
mjarkk <mkopenga@gmail.com>
2025-12-03 07:56:02 +0100

    reduce code

--- day_03/app/Main.hs
+++ day_03/app/Main.hs
@@ -5,33 +5,23 @@ import System.CPUTime
 main :: IO ()
 main = do
   start <- getCPUTime
-
   puzzleContents <- readFile "puzzle.txt"
   print $ solve puzzleContents
-
   end <- getCPUTime
-  let diff = round (fromIntegral (end - start) / 1000000000 :: Double)
+  let diff = (fromIntegral (end - start) / 1000000000 :: Double)
   putStrLn ("Execution time: " ++ show diff ++ " ms")
 
 parse :: String -> [[Int]]
-parse input = map (\l -> map (\c -> read [c] :: Int) l) $ filter (/= "") $ lines input
+parse input = map (map (\c -> read [c])) $ filter (/= "") $ lines input
 
 solve :: String -> (Int, Int)
-solve puzzleInput =
-  let parsedPuzzle = parse puzzleInput
-   in foldl (\(p1, p2) row -> (p1 + solveRow 2 row, p2 + solveRow 12 row)) (0, 0) parsedPuzzle
+solve puzzleInput = foldl (\(p1, p2) row -> (p1 + solveRow 2 row, p2 + solveRow 12 row)) (0, 0) $ parse puzzleInput
 
 solveRow :: Int -> [Int] -> Int
-solveRow n row =
-  let solution = read $ concat $ map show $ fst $ foldl (\acc idx -> takeHighestNum acc idx n row) ([], 0) [0 .. n - 1]
-   in solution
+solveRow n row = read $ concatMap show $ fst $ foldl (\acc idx -> takeHighestNum acc idx n row) ([], 0) [0 .. n - 1]
 
 takeHighestNum :: ([Int], Int) -> Int -> Int -> [Int] -> ([Int], Int)
 takeHighestNum (accList, accOffset) idx n row =
-  let dropFromEnd = n - idx - 1
-      searchSpace = drop accOffset $ take (length row - dropFromEnd) row
-      (val, valIdx) = highestNumberAt searchSpace
+  let searchSpace = drop accOffset $ take (length row - (n - idx - 1)) row
+      (val, valIdx) = foldl (\(maxVal, maxIdx) (fv, fi) -> if fv > maxVal then (fv, fi) else (maxVal, maxIdx)) (-1, -1) $ zip searchSpace [0 ..]
    in (accList ++ [val], accOffset + valIdx + 1)
-
-highestNumberAt :: [Int] -> (Int, Int) -- (val, idx)
-highestNumberAt row = foldl (\(maxVal, maxIdx) (val, idx) -> if val > maxVal then (val, idx) else (maxVal, maxIdx)) (-1, -1) $ zip row [0 ..]

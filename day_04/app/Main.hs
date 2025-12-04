a035cf1a46a62d90cc20131a1c4a4cd1ae7311ff
mjarkk <mkopenga@gmail.com>
2025-12-04 08:11:13 +0100

    speeeed

--- day_04/app/Main.hs
+++ day_04/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Debug.Trace
+import Data.Array.Unboxed
 import System.CPUTime
 
 main :: IO ()
@@ -16,28 +16,27 @@ solve :: String -> (Int, Int)
 solve input =
   let warehouse = filter (/= "") $ lines input
       mapHeight = length warehouse
-      mapWidth = length (head warehouse)
-   in solveR warehouse mapHeight mapWidth
+      mapWidth = length $ head warehouse
+      warehouseArray = listArray ((0, 0), (mapHeight - 1, mapWidth - 1)) [c | row <- warehouse, c <- row] :: UArray (Int, Int) Char
+   in solveR warehouseArray mapHeight mapWidth
 
-solveR :: [String] -> Int -> Int -> (Int, Int)
+solveR :: UArray (Int, Int) Char -> Int -> Int -> (Int, Int)
 solveR warehouse mapHeight mapWidth =
-  let toRemove = map (\(row, y) -> map (\(c, x) -> c == '@' && canRemoveRollOfPaper warehouse (x, y)) $ zip row [0 .. mapWidth - 1]) $ zip warehouse [0 .. mapHeight - 1]
-      totalRemoved = foldl (\acc x -> if x then acc + 1 else acc) 0 $ concat toRemove
-   in (totalRemoved, if totalRemoved > 0 then totalRemoved + snd (solveR (removeFromMap warehouse toRemove) mapHeight mapWidth) else 0)
+  let toRemove = [(y, x) | y <- [0 .. mapHeight - 1], x <- [0 .. mapWidth - 1], warehouse ! (y, x) == '@' && canRemoveRollOfPaper warehouse (x, y)]
+      removed = length toRemove
+   in if removed > 0
+        then
+          let newWarehouse = warehouse // [((y, x), '.') | (y, x) <- toRemove]
+           in (removed, removed + snd (solveR newWarehouse mapHeight mapWidth))
+        else (0, 0)
 
-removeFromMap :: [String] -> [[Bool]] -> [String]
-removeFromMap warehouse toRemove = map (\(mapRow, removeRow) -> map (\(c, r) -> if r then '.' else c) $ zip mapRow removeRow) $ zip warehouse toRemove
-
-canRemoveRollOfPaper :: [String] -> (Int, Int) -> Bool
+canRemoveRollOfPaper :: UArray (Int, Int) Char -> (Int, Int) -> Bool
 canRemoveRollOfPaper warehouse (x, y) =
-    let neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x + 1, y - 1), (x - 1, y + 1), (x + 1, y + 1)]
-        neighborValues = foldl (\acc (x', y') -> if isRolOfPaper warehouse (x', y') then acc + 1 else acc) 0 neighbors
-     in neighborValues < 4
+  let neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x + 1, y - 1), (x - 1, y + 1), (x + 1, y + 1)]
+      neighborValues = foldl (\acc (x', y') -> if isRolOfPaper warehouse (x', y') then acc + 1 else acc) 0 neighbors
+   in neighborValues < 4
 
-isRolOfPaper :: [String] -> (Int, Int) -> Bool
-isRolOfPaper warehouse (x, y) = atMapPosition warehouse (x, y) == '@'
-
-atMapPosition :: [String] -> (Int, Int) -> Char
-atMapPosition warehouse (x, y)
-  | x >= 0 && x < length (head warehouse) && y >= 0 && y < length warehouse = warehouse !! y !! x
-  | otherwise = '.'
+isRolOfPaper :: UArray (Int, Int) Char -> (Int, Int) -> Bool
+isRolOfPaper warehouse (x, y) =
+  let ((minX, minY), (maxX, maxY)) = bounds warehouse
+   in x >= minX && x <= maxX && y >= minY && y <= maxY && warehouse ! (x, y) == '@'

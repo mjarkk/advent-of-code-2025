7930cdcdc117213fc2bf429dbf40086d99f608dc
mjarkk <mkopenga@gmail.com>
2025-12-11 09:02:39 +0100

    solve day 11

---
+++ day_11/go.mod
@@ -0,0 +1,3 @@
+module github.com/mjarkk/day_11
+
+go 1.25.4
---
+++ day_11/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Node struct {
+	Name          string
+	Links         []*Node
+	CachedResults map[*Node]int
+}
+
+func main() {
+	start := time.Now()
+	contents, err := os.ReadFile("puzzle.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	nodes := map[string]*Node{}
+	for line := range strings.SplitSeq(string(contents), "\n") {
+		if line == "" {
+			continue
+		}
+
+		nameAndChildren := strings.SplitN(line, ": ", 2)
+		name := nameAndChildren[0]
+		node := nodes[name]
+		if node == nil {
+			node = &Node{Name: name}
+			nodes[name] = node
+		}
+
+		for childName := range strings.SplitSeq(nameAndChildren[1], " ") {
+			childNode := nodes[childName]
+			if childNode == nil {
+				childNode = &Node{Name: childName}
+				nodes[childName] = childNode
+			}
+			node.Links = append(node.Links, childNode)
+		}
+	}
+
+	v := &Visitor{
+		OUT: nodes["out"],
+		DAC: nodes["dac"],
+		FFT: nodes["fft"],
+	}
+
+	you := nodes["you"]
+	if you != nil {
+		answerP1 := v.FindOutsViaDac(you, v.OUT)
+		fmt.Println("p1:", answerP1)
+	}
+
+	svr := nodes["svr"]
+	if svr != nil {
+		answerP2 := v.FindOutsViaDac(svr, nil)
+		fmt.Println("p2:", answerP2)
+	}
+
+	fmt.Println("in", time.Since(start))
+}
+
+type Visitor struct {
+	OUT *Node
+	DAC *Node
+	FFT *Node
+}
+
+func (v *Visitor) FindOutsViaDac(node *Node, findNext *Node) int {
+	resultsCache, ok := node.CachedResults[findNext]
+	if ok {
+		return resultsCache
+	}
+
+	resp := 0
+	for _, link := range node.Links {
+		switch link {
+		case v.DAC:
+			switch findNext {
+			case nil:
+				resp += v.FindOutsViaDac(link, v.FFT)
+			case v.DAC:
+				resp += v.FindOutsViaDac(link, v.OUT)
+			}
+		case v.FFT:
+			switch findNext {
+			case nil:
+				resp += v.FindOutsViaDac(link, v.DAC)
+			case v.FFT:
+				resp += v.FindOutsViaDac(link, v.OUT)
+			}
+		case v.OUT:
+			if findNext == v.OUT {
+				resp += 1
+			}
+		default:
+			resp += v.FindOutsViaDac(link, findNext)
+		}
+	}
+
+	if node.CachedResults == nil {
+		node.CachedResults = map[*Node]int{findNext: resp}
+	} else {
+		node.CachedResults[findNext] = resp
+	}
+
+	return resp
+}

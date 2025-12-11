43d8879416139699d6a53d7b890f584d4dade553
mjarkk <mkopenga@gmail.com>
2025-12-11 18:55:54 +0100

    improve speed of day 8 in golang

--- day_08_golang/main.go
+++ day_08_golang/main.go
@@ -10,6 +10,11 @@ import (
 	"time"
 )
 
+const (
+	TOTAL_BUCKETS = 10
+	BUCKET_SIZE   = 100_000 / TOTAL_BUCKETS
+)
+
 type Network struct {
 	Parent *Network
 }
@@ -46,7 +51,8 @@ func main() {
 		panic(err)
 	}
 
-	boxes := make([]*JunctionBox, 0, 1_000)
+	boxes := [TOTAL_BUCKETS][TOTAL_BUCKETS][TOTAL_BUCKETS][]*JunctionBox{}
+	totalNetworks := uint16(0)
 	links := make([]Link, 0, 6_000)
 	for line := range strings.SplitSeq(string(contents), "\n") {
 		if line == "" {
@@ -68,20 +74,38 @@ func main() {
 				float64(cord[1]),
 				float64(cord[2]),
 			},
+			Network: &Network{},
 		}
 
-		for _, boxNeedle := range boxes {
-			distance := distrance(box, boxNeedle)
-			if distance < 15_000.0 {
-				links = append(links, Link{
-					Distance: distance,
-					A:        box,
-					B:        boxNeedle,
-				})
+		xk, yk, zk := boxKey(cord)
+		for i := xk - 1; i <= xk+1; i++ {
+			if i < 0 || i >= TOTAL_BUCKETS {
+				continue
+			}
+			for j := yk - 1; j <= yk+1; j++ {
+				if j < 0 || j >= TOTAL_BUCKETS {
+					continue
+				}
+				for k := zk - 1; k <= zk+1; k++ {
+					if k < 0 || k >= TOTAL_BUCKETS {
+						continue
+					}
+					for _, boxNeedle := range boxes[i][j][k] {
+						distance := distrance(box, boxNeedle)
+						if distance < 15_000.0 {
+							links = append(links, Link{
+								Distance: distance,
+								A:        box,
+								B:        boxNeedle,
+							})
+						}
+					}
+				}
 			}
 		}
 
-		boxes = append(boxes, box)
+		boxes[xk][yk][zk] = append(boxes[xk][yk][zk], box)
+		totalNetworks++
 	}
 
 	slices.SortFunc(links, func(a Link, b Link) int {
@@ -94,11 +118,6 @@ func main() {
 		return 0
 	})
 
-	for _, box := range boxes {
-		box.Network = &Network{}
-	}
-	totalNetworks := uint16(len(boxes))
-
 	for _, link := range links {
 		if link.A.Network.Root() != link.B.Network.Root() {
 			totalNetworks--
@@ -106,7 +125,7 @@ func main() {
 			link.B.Network.Root().Parent = link.A.Network.Root()
 			if totalNetworks == 1 {
 				answer := link.A.X * link.B.X
-				fmt.Println("part2:", answer /* , answer == uint64(9003685096)*/)
+				fmt.Println("part2:", answer, answer == uint64(9003685096))
 				break
 			}
 		}
@@ -120,4 +139,11 @@ func distrance(a *JunctionBox, b *JunctionBox) float64 {
 	dy := a.Cord[1] - b.Cord[1]
 	dz := a.Cord[2] - b.Cord[2]
 	return math.Sqrt(dx*dx + dy*dy + dz*dz)
+}
+
+func boxKey(cord [3]uint64) (int8, int8, int8) {
+	x := int8(cord[0] / BUCKET_SIZE)
+	y := int8(cord[1] / BUCKET_SIZE)
+	z := int8(cord[2] / BUCKET_SIZE)
+	return x, y, z
 }
